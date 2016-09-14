382ae6a5c55bf7b77dc3243a97a9cdd24e7c8c92
Jaden B <jjball@users.noreply.github.com>
2016-09-14 14:55:58 -0600

    Cleaned up comments

--- app/src/main/java/com/jadenball/simpletodolist/PopDeleteItem.java
+++ app/src/main/java/com/jadenball/simpletodolist/PopDeleteItem.java
@@ -22,17 +22,15 @@ public class PopDeleteItem extends Activity {
 
         getWindow().setLayout( (int)(width * 0.6), (int)(height * 0.15));
 
-        //void cancelPopUpActivity(){
-        //  Pop.this.finish();
-        // }
-
     }
 
+    //When user presses the cancel button on the dialog, sets the result to cancelled to do nothing
     public void cancelDeleteTask(View view) {
         setResult(RESULT_CANCELED);
         finish();
     }
 
+    //When user presses the delete button, send the ok in the result to delete the string from the list/Database
     public void deleteTheTask(View view) {
         setResult(RESULT_OK);
         finish();
